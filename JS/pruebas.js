b737887547f45fdb52f5e82bfea11de06eec140b
jpkuro <joseluispalmapiguave@gmail.com>
2026-06-15 23:37:19 -0500

    ejercicio basico javascript

--- JS/pruebas.js
+++ JS/pruebas.js
@@ -1,1 +1,74 @@
-alert("prueba de alerta");
+/*var nombre = "Jose";
+var edad = 29;
+var presenteONo = true;
+var apellido = "Palma";
+var statura = 1.67;
+//let nombre2 = "Luis";
+
+//alert("el nombre es:" + nombre);
+
+console.log("el nombre es:" + nombre + " " + typeof nombre);
+console.log("la edad es:" + edad + " " + typeof edad);
+console.log("presente o no:" + presenteONo + " " + typeof presenteONo);
+console.log("el apellido es:" + apellido + " " + typeof apellido);
+console.log("la statura es:" + statura + " " + typeof statura);
+
+if(true){
+    let nombre2 = "Luis";
+}
+
+console.log("el nombre es:" + nombre2 );*/
+
+/*var suma = 5 + 3;
+var resta = 5 - 3;
+var multiplicacion = 5 * 3;
+var division = 5 / 3;
+var modulo = 5 % 3;
+
+console.log("suma:" + suma + " " + typeof suma);
+console.log("resta:" + resta + " " + typeof resta);
+console.log("multiplicación:" + multiplicacion + " " + typeof multiplicacion);
+console.log("división:" + division + " " + typeof division);
+console.log("módulo:" + modulo + " " + typeof modulo);*/
+/*
+console.log(5>2);
+console.log(98<1);
+console.log(5=5);*/
+
+/*var edad = 14;
+var DNI = true;
+
+if(edad >= 18 && DNI == true){
+    console.log("puede votar");
+}else{
+    console.log("no puede votar");
+}*/
+/*var edad = 14;
+var DNI = true;
+
+if(edad >= 18 || DNI == true){
+    console.log("puede votar");
+}else{
+    console.log("no puede votar");
+}*/
+/*var edad = 14;
+var DNI = !true;
+
+if(edad >= 18 || DNI == true){
+    console.log("puede votar");
+}else{
+    console.log("no puede votar");
+}
+
+*/
+
+var comensal = "Jose";
+var cuentaComida = 2800 + 1300;
+var cuentaBebida = 900 + 650;
+
+var totalCuenta = cuentaComida + cuentaBebida;
+
+console.log("el total de la cuenta en comida es de: " + cuentaComida);
+console.log("El total de la cuenta en bebida es de: " + cuentaBebida);
+console.log("El total de la cuenta es de: " + totalCuenta);
+
